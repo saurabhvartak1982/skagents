28b6ba1afd0f7f97ddd2f0780ad97c7ef06fac77
saurabh <saurabh_vartak@outlook.com>
2025-02-17 20:41:11 +0530

    added Agent5 related code to do the JSON validation using plugin

--- server_json.py
+++ server_json.py
@@ -2,13 +2,106 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from semantic_kernel import Kernel
+from semantic_kernel.connectors.ai import FunctionChoiceBehavior
 from semantic_kernel.agents import ChatCompletionAgent
 from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
 from semantic_kernel.contents import ChatHistory
-from semantic_kernel.functions import KernelArguments
+from semantic_kernel.functions import KernelArguments, kernel_function
+from semantic_kernel.contents.function_call_content import FunctionCallContent
+from semantic_kernel.contents.function_result_content import FunctionResultContent
 from semantic_kernel.kernel_pydantic import KernelBaseModel # Introduced for Structured Output
 from semantic_kernel.prompt_template import PromptTemplateConfig # Introduced for Prompt Template
 import yaml # Introduced for Prompt Template
+
+# Observability related imports
+import logging
+from azure.monitor.opentelemetry.exporter import (
+    AzureMonitorLogExporter,
+    AzureMonitorMetricExporter,
+    AzureMonitorTraceExporter,
+)
+
+from opentelemetry._logs import set_logger_provider
+from opentelemetry.metrics import set_meter_provider
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.sdk.metrics.view import DropAggregation, View
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.semconv.resource import ResourceAttributes
+from opentelemetry.trace import set_tracer_provider
+# End of observability related imports
+
+# Telemetry setup code goes here
+
+# Replace the connection string with your Application Insights connection string
+connection_string = "<App-Insights connection string here>"
+
+# Create a resource to represent the service/sample
+resource = Resource.create({ResourceAttributes.SERVICE_NAME: "single_agent_server"})
+
+
+def set_up_logging():
+    exporter = AzureMonitorLogExporter(connection_string=connection_string)
+
+    # Create and set a global logger provider for the application.
+    logger_provider = LoggerProvider(resource=resource)
+    # Log processors are initialized with an exporter which is responsible
+    # for sending the telemetry data to a particular backend.
+    logger_provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
+    # Sets the global default logger provider
+    set_logger_provider(logger_provider)
+
+    # Create a logging handler to write logging records, in OTLP format, to the exporter.
+    handler = LoggingHandler()
+    # Add filters to the handler to only process records from semantic_kernel.
+    handler.addFilter(logging.Filter("semantic_kernel"))
+    # Attach the handler to the root logger. `getLogger()` with no arguments returns the root logger.
+    # Events from all child loggers will be processed by this handler.
+    logger = logging.getLogger()
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+
+def set_up_tracing():
+    exporter = AzureMonitorTraceExporter(connection_string=connection_string)
+
+    # Initialize a trace provider for the application. This is a factory for creating tracers.
+    tracer_provider = TracerProvider(resource=resource)
+    # Span processors are initialized with an exporter which is responsible
+    # for sending the telemetry data to a particular backend.
+    tracer_provider.add_span_processor(BatchSpanProcessor(exporter))
+    # Sets the global default tracer provider
+    set_tracer_provider(tracer_provider)
+
+
+def set_up_metrics():
+    exporter = AzureMonitorMetricExporter(connection_string=connection_string)
+
+    # Initialize a metric provider for the application. This is a factory for creating meters.
+    meter_provider = MeterProvider(
+        metric_readers=[PeriodicExportingMetricReader(exporter, export_interval_millis=5000)],
+        resource=resource,
+        views=[
+            # Dropping all instrument names except for those starting with "semantic_kernel"
+            View(instrument_name="*", aggregation=DropAggregation()),
+            View(instrument_name="semantic_kernel*"),
+        ],
+    )
+    # Sets the global default meter provider
+    set_meter_provider(meter_provider)
+
+
+# This must be done before any other telemetry calls
+set_up_logging()
+set_up_tracing()
+set_up_metrics()
+
+# End of telemetry setup code
+
 
 # FastAPI App
 app = FastAPI()
@@ -24,6 +117,25 @@ class ChatRequest(BaseModel):
 # For Structured Output
 class Step(KernelBaseModel):
     message: str
+
+class Utilities:
+    """A utility class which will have multiple functions which can be used by the Agents."""
+
+    @kernel_function(description="Checks if the given JSON string is valid and adheres to the structure defined by the class Step")
+    def validate_json(self, json_str: str) -> bool:
+        """
+        Attempts to parse the given JSON string into a Step instance.
+        Returns True if the JSON adheres to the Step structure, otherwise False.
+        """
+        try:
+            # Attempt to parse the JSON string into a Step instance.
+            step = Step.model_validate_json(json_str)
+            # print("Valid JSON. Parsed Step instance:", step)
+            print("From the plug-in: Valid JSON")
+            return True
+        except Exception as e:  # Catching any exception since we don't have access to a specific ValidationError
+            print("From the plug-in: Invalid JSON or schema mismatch:", e)
+            return False
 
 
 @app.post("/agent1")
@@ -170,6 +282,39 @@ async def chat_with_agent(request: ChatRequest):
             "user_input": request.message,
             "agent_response": step.message
         }
+
+@app.post("/agent5")
+async def chat_with_agent(request: ChatRequest):
+    """
+    Handles user input and returns the agent5's response.
+    """
+    AGENT5_SERVICEID = "agent5"
+
+    kernel.add_service(AzureChatCompletion(service_id=AGENT5_SERVICEID))
+    kernel.add_plugin(Utilities(), plugin_name="utilities")
+    agent5Settings = kernel.get_prompt_execution_settings_from_service_id(service_id=AGENT5_SERVICEID)
+    agent5Settings.function_choice_behavior = FunctionChoiceBehavior.Auto()
+    agent5Settings.response_format=Step
+
+    # Define the Agent
+    AGENT_NAME = "Agent5"
+    AGENT_INSTRUCTIONS = """You are a helpful intelligent agent that repeats the user message as an agent no.5 from the movie The Matrix. Please mention the agent number in your response.
+    You will invoke the LLM to fetch the response and then make use of the function validate_json to validate the JSON structure of the response. 
+    If validate_json returns True, it means that the JSON is valid. If it returns False, the JSON is invalid.
+    If the JSON structure is invalid, you will prompt the LLM to correct the response. If the JSON structure doesnt get corrected in 3 attempts respond with an error message - All agents are busy, please try later."""
+    #You will attempt to correct the response 3 times based on the output of validate_json. If the correction attempts exceed 3 and your are still not able to fetch the response in the correct JSON schema, you will send an error message."""
+    agent = ChatCompletionAgent(service_id=AGENT5_SERVICEID, kernel=kernel, name=AGENT_NAME, instructions=AGENT_INSTRUCTIONS, arguments=KernelArguments(settings=agent5Settings))
+
+    chat_history = ChatHistory()
+    chat_history.add_user_message(request.message)
+
+    response_text = ""
+    async for content in agent.invoke(chat_history):
+        # step = Step.model_validate_json(content.content)
+        chat_history.add_message(content) # Store the last response
+        response_text = content.content  
+
+    return {"user_input": request.message, "agent_response": response_text}
 
 
 
